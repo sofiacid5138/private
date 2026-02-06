db9cb4e8935d320bd34054dc73cd15afc564771a
diego.alejandre5117@alumnos.udg.mx <jose.galvan1010@alumnos.udg.mx>
2026-02-06 12:29:32 -0600

    LandingPage_2

--- dreamybunny.py
+++ dreamybunny.py
@@ -6,5 +6,9 @@ dreamybunnyApp = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@dreamybunnyApp.route('/signup',methods=['GET','POST'])
+def signup():
+    return render_template('signup.html')
+
 if __name__ == '__main__':
     dreamybunnyApp.run(port=2200,debug=True)
