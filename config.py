6f204dbcb6aa104aaa913797bd61ded4173684ba
edwinsaldana5139 <edwin.saldana5139@alumnos.udg.mx>
2026-02-11 12:46:47 -0600

    sql_p8

---
+++ config.py
@@ -0,0 +1,13 @@
+class Config:
+    SECRET_KEY  = "trestristestrigrestragabantrigoenuntrigal"
+    DEBUG       =True
+
+class DevelopmentConfig(Config):
+    MYSQL_HOST      = 'localhost'
+    MYSQL_USER      ='root'
+    MYSQL_PASSWORD  ='mysql'
+    MYSQL_DB        = 'steetkick'
+
+config = {
+    'deveploment':DevelopmentConfig
+}
--- dreamybunny.py
+++ dreamybunny.py
@@ -1,15 +1,18 @@
 from flask import Flask,render_template,url_for,request
+from flask_mysqldb import MySQL
 from werkzeug.security import generate_password_hash
+from config import  config
 
 dreamybunnyApp = Flask(__name__)
+db             =MySQL(dreamybunnyApp)
 
 @dreamybunnyApp.route('/')
 def home():
-    return render_template('home.html')
+
 
 @dreamybunnyApp.route('/signup',methods=['GET','POST'])
 def signup():
-    return render_template('signup.html')
+    
 
 if request.form ==  'POST':
     nombre = request.form['nombre']
@@ -24,6 +27,6 @@ if request.form ==  'POST':
 
 else:
 
-
 if __name__ == '__main__':
+    @dreamybunnyApp.config.fromobject(config[development])
     dreamybunnyApp.run(port=2200,debug=True)
