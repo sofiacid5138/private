87375edaaf4f0d5f789b0c99c74850aa1d52a876
ulises.rodriguez5127@alumnos.udg.mx <gabriela.garcia1864@alumnos.udg.mx>
2026-02-16 10:41:19 -0600

    signup2

--- dreamybunny.py
+++ dreamybunny.py
@@ -2,32 +2,32 @@
 from flask import Flask,render_template,url_for,request
 from flask_mysqldb import MySQL
 from werkzeug.security import generate_password_hash
-from config import Config
+from config import config
 
 dreamybunnyApp = Flask(__name__)
 db             =MySQL(dreamybunnyApp)
 
 @dreamybunnyApp.route('/')
 def home():
+    return render_template('home.html')
 
 
 @dreamybunnyApp.route('/signup',methods=['GET','POST'])
 def signup():
-
-
-if request.form ==  'POST':
-    nombre = request.form['nombre']
-    correo = request.form['correo']
-    clave = requets.form ['clave']
-    claveCifrada = generate_password_hash(clave)
-    regUsuario = db.connection.cursor()
-    regUsuario.execute("INSERT INTO usuario (nombre, correo, clave) VALUES (%s, %s, %s)",(nombre.upper(), correo, claveCifrada))
-    db.connection.commit()
-    regUsuario.close()
-    return render_template('home.html')
-else:
-    return render_template('signup.html')
+    if request.method ==  'POST':
+        nombre = request.form['nombre']
+        correo = request.form['correo']
+        clave = request.form ['clave']
+        claveCifrada = generate_password_hash(clave)
+        regUsuario = db.connection.cursor()
+        regUsuario.execute("INSERT INTO usuario (nombre, correo, clave) VALUES (%s, %s, %s)",(nombre.upper(), correo, claveCifrada))
+        db.connection.commit()
+        regUsuario.close()
+        db.connection.commit()
+        return render_template('home.html')
+    else:
+        return render_template('signup.html')
 
 if __name__ == '__main__':
-    @dreamybunnyApp.config.fromobject(config[development])
-    dreamybunnyApp.run(port=2200,debug=True)
+    dreamybunnyApp.config.from_object(config['development'])
+    dreamybunnyApp.run(port=2200)
